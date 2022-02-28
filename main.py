12b2d9c9a3f5855030ff14bc9824ce47ae3fe5b7
hx <495921109@qq.com>
2022-02-28 21:54:14 +0800

    local model user prefence model

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     n=5
