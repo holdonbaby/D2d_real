8a14764061610fbb6bf17fd0de060e4da90b1cad
hx <495921109@qq.com>
2022-03-04 23:09:25 +0800

    local model user prefence model zipf 多喜捏 EM

---
+++ test/scipt.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+mu = 30  # mean of distribution
+sigma = 2  # standard deviation of distribution
+x = mu + sigma * np.random.randn(10000)
+
+
+def mle(x):
+    """
+    极大似然估计
+    :param x:
+    :return:
+    """
+    u = np.mean(x)
+    return u, np.sqrt(np.dot(x - u, (x - u).T) / x.shape[0])
+
+
+print(mle(x))
+num_bins = 100
+plt.hist(x, num_bins)
+plt.show()
+
--- userpreference/timewindow.py
+++ userpreference/timewindow.py
@@ -6,6 +6,8 @@ ted Wireless  D2D  Caching  Networks[C] 2020 IEEE/CIC International Conference o
 这篇论文提出了一个叫做EM （ Expectation Maximization）算法来学习用户偏好，并通过仿真证明了预测的准确性较高
 采用滑动窗口法预测实时用户活跃程度，联合考虑用户偏好和实时用户活跃程度来预测实时文件流行度
 
+用户偏好学习算法流程
+
 t 时间
 f 文件  F
 k 用户  U
@@ -16,6 +18,13 @@ active 用户活跃程度
 popular 流行程度
 like 喜好程度
 probability 概率
+
+为了生成用户的个人偏好概率，首先分别通过其对应的分布来生
+成个人流行度分布和排名顺序，然后通过这两个分布来生成所需的概率
+
+个人流行度分布:大小分布  个人类型流行度分布  基于类型的条件流行度分布
+
+个人排名顺序的统计信息：大小分布  类型偏好概率  类型排名分布  文件排名分布
 """
 
 
@@ -39,6 +48,7 @@ def popular_f_t(active_t, preference_f_t):
         ans += active_t[i] * preference_f_t[i]
 
     return ans
+
 
 
 
