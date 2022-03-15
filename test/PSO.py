916ea006eb8f3d5786b470b0e83d9bb64b004db3
hx <495921109@qq.com>
2022-03-15 17:24:37 +0800

    GWO PSO new

--- test/PSO.py
+++ test/PSO.py
@@ -1,109 +1,81 @@
-# -*- coding:utf-8 -*-
+#基本粒子群算法
+#vi+1 = w*vi+c1*r1*(pi-xi)+c2*r2*(pg-xi)   速度更新公式
+#xi+1 = xi + a*vi+1    位置更新公式（一般a=1）
+#w = wmax -(wmax-wmin)*iter/Iter  权重更新公式
+#iter当前迭代次数 Iter最大迭代次数 c1、c2学习因子  r1、r2随机数 pi粒子当前最优位置  pg粒子群全局最优
+#初始化 wmax=0.9 wmin=0.4 通常c1=c2=2 Iter对于小规模问题（10，20）对于大规模（100，200）
+#算法优劣取决于w、c1和c2，迭代结束的条件是适应度函数的值符合具体问题的要求
+#初始化粒子群，包括尺寸、速度和位置
+#本算法假设想要的输出是长度为10的矩阵，y=[1.7]*10,适应度函数f（x）= |x-y| <=0.001符合要求
+
 import numpy as np
-import matplotlib.pyplot as plt
-import math
 
-def getweight():
-    # 惯性权重
-    weight = 1
-    return weight
+swarmsize = 500
+partlen = 10
+wmax,wmin = 0.9,0.4
+c1 = c2 = 2
+Iter = 400
 
-def getlearningrate():
-    # 分别是粒子的个体和社会的学习因子，也称为加速常数
-    lr = (0.49445,1.49445)
-    return lr
+def getwgh(iter):
+    w = wmax - (wmax-wmin)*iter/Iter
+    return w
 
-def getmaxgen():
-    # 最大迭代次数
-    maxgen = 300
-    return maxgen
+def getrange():
+    randompv = (np.random.rand()-0.5)*2
+    return randompv
 
-def getsizepop():
-    # 种群规模
-    sizepop = 50
-    return sizepop
+def initswarm():
+    vswarm,pswarm = np.zeros((swarmsize,partlen)),np.zeros((swarmsize,partlen))
+    for i in range(swarmsize):
+        for j in range(partlen):
+            vswarm[i][j] = getrange()
+            pswarm[i][j] = getrange()
+    return vswarm,pswarm
 
-def getrangepop():
-    # 粒子的位置的范围限制,x、y方向的限制相同
-    rangepop = (-2*math.pi , 2*math.pi)
-    #rangepop = (-2,2)
-    return rangepop
+def getfitness(pswarm):
+    pbest = np.zeros(partlen)
+    fitness = np.zeros(swarmsize)
+    for i in range(partlen):
+        pbest[i] = 1.7
 
-def getrangespeed():
-    # 粒子的速度范围限制
-    rangespeed = (-0.5,0.5)
-    return rangespeed
+    for i in range(swarmsize):
+        yloss = pswarm[i] - pbest
+        for j in range(partlen):
+            fitness[i] += abs(yloss[j])
+    return fitness
 
-def func(x):
-    # x输入粒子位置
-    # y 粒子适应度值
-    if (x[0]==0)&(x[1]==0):
-        y = np.exp((np.cos(2*np.pi*x[0])+np.cos(2*np.pi*x[1]))/2)-2.71289
-    else:
-        y = np.sin(np.sqrt(x[0]**2+x[1]**2))/np.sqrt(x[0]**2+x[1]**2)+np.exp((np.cos(2*np.pi*x[0])+np.cos(2*np.pi*x[1]))/2)-2.71289
-    return y
+def getpgfit(fitness,pswarm):
+    pgfitness = fitness.min()
+    pg = pswarm[fitness.argmin()].copy()
+    return pg,pgfitness
 
-def initpopvfit(sizepop):
-    pop = np.zeros((sizepop,2))
-    v = np.zeros((sizepop,2))
-    fitness = np.zeros(sizepop)
+vswarm,pswarm = initswarm()
+fitness = getfitness(pswarm)
+pg,pgfit = getpgfit(fitness,pswarm)
+pi,pifit = pswarm.copy(),fitness.copy()
 
-    for i in range(sizepop):
-        pop[i] = [(np.random.rand()-0.5)*rangepop[0]*2,(np.random.rand()-0.5)*rangepop[1]*2]
-        v[i] = [(np.random.rand()-0.5)*rangepop[0]*2,(np.random.rand()-0.5)*rangepop[1]*2]
-        fitness[i] = func(pop[i])
-    return pop,v,fitness
+for iter in range(Iter):
+    if pgfit <= 0.001:
+        break
+    #更新速度和位置
+    weight = getwgh(iter)
+    for i in range(swarmsize):
+        for j in range(partlen):
+            vswarm[i][j] = weight*vswarm[i][j] + c1*np.random.rand()*(pi[i][j]-pswarm[i][j]) + c2*np.random.rand()*(pg[j]-pswarm[i][j])
+            pswarm[i][j] = pswarm[i][j] + vswarm[i][j]
+    #更新适应值
+    fitness = getfitness(pswarm)
+    #更新全局最优粒子
+    pg,pgfit = getpgfit(fitness,pswarm)
+    #更新局部最优粒子
+    for i in range(swarmsize):
+        if fitness[i] < pifit[i]:
+            pifit[i] = fitness[i].copy()
+            pi[i] = pswarm[i].copy()
 
-def getinitbest(fitness,pop):
-    # 群体最优的粒子位置及其适应度值
-    gbestpop,gbestfitness = pop[fitness.argmax()].copy(),fitness.max()
-    #个体最优的粒子位置及其适应度值,使用copy()使得对pop的改变不影响pbestpop，pbestfitness类似
-    pbestpop,pbestfitness = pop.copy(),fitness.copy()
-
-    return gbestpop,gbestfitness,pbestpop,pbestfitness
-
-w = getweight()
-lr = getlearningrate()
-maxgen = getmaxgen()
-sizepop = getsizepop()
-rangepop = getrangepop()
-rangespeed = getrangespeed()
-
-pop,v,fitness = initpopvfit(sizepop)
-gbestpop,gbestfitness,pbestpop,pbestfitness = getinitbest(fitness,pop)
-
-result = np.zeros(maxgen)
-for i in range(maxgen):
-        t=0.5
-        #速度更新
-        for j in range(sizepop):
-            v[j] += lr[0]*np.random.rand()*(pbestpop[j]-pop[j])+lr[1]*np.random.rand()*(gbestpop-pop[j])
-        v[v<rangespeed[0]] = rangespeed[0]
-        v[v>rangespeed[1]] = rangespeed[1]
-
-        #粒子位置更新
-        for j in range(sizepop):
-            #pop[j] += 0.5*v[j]
-            pop[j] = t*(0.5*v[j])+(1-t)*pop[j]
-        pop[pop<rangepop[0]] = rangepop[0]
-        pop[pop>rangepop[1]] = rangepop[1]
-
-        #适应度更新
-        for j in range(sizepop):
-            fitness[j] = func(pop[j])
-
-        for j in range(sizepop):
-            if fitness[j] > pbestfitness[j]:
-                pbestfitness[j] = fitness[j]
-                pbestpop[j] = pop[j].copy()
-
-        if pbestfitness.max() > gbestfitness :
-            gbestfitness = pbestfitness.max()
-            gbestpop = pop[pbestfitness.argmax()].copy()
-
-        result[i] = gbestfitness
-
-
-plt.plot(result)
-plt.show()
-print(result)
+for j in range(swarmsize):
+    if pifit[j] < pgfit:
+        pgfit = pifit[j].copy()
+        pg = pi[j].copy()
+print(pg)
+print(pgfit)
